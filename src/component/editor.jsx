063fd4edcda39213ba4bce7929f1b9b0ea7577c0
Tarun kumar singh <tarunks2512@gmail.com>
2020-10-27 00:12:28 +0530

    get data

--- src/component/editor.jsx
+++ src/component/editor.jsx
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-
+let textEditor 
+const submitText = () =>{
+console.log(textEditor.getData())
+}
 function Editor(){
     return(
         <div className="App">
         <h2>Using CKEditor 5 build in React</h2>
+        <button onClick={() => submitText()}>submit</button>
         <CKEditor
             editor={ ClassicEditor }
             // data="<p>Hello from CKEditor 5!</p>"
             onInit={ editor => {
                 // You can store the "editor" and use when it is needed.
                 console.log( 'Editor is ready to use!', editor );
+                textEditor = editor
             } }
             onChange={ ( event, editor ) => {
                 const data = editor.getData();
