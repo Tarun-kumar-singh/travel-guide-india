6924622ac141993c59962178bcac3e39f1803294
Tarun kumar singh <tarunks2512@gmail.com>
2020-10-31 01:38:27 +0530

    loader

--- src/component/quiz-question/displayQuestions.jsx
+++ src/component/quiz-question/displayQuestions.jsx
@@ -6,15 +6,28 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
 function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
   }
-  
+  const useStyles = makeStyles((theme) => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+  }));
 const DisplayQuestion = () => {
+
+    const classes = useStyles();
     const [open, setOpen] = useState(false);
     const [error, setError] = useState(false)
+    const [loader, setLoader] = useState(false)
+
     const correctAsnwer = 1
+
     const handleChange = (event) => {
         if(event.target.value == correctAsnwer){
             setError(false)
@@ -24,6 +37,7 @@ const DisplayQuestion = () => {
             setOpen(false)
             setError(true)
         }
+        setLoader(true)
     };
 
      const handleClose = (event, reason) => {
@@ -60,6 +74,10 @@ const DisplayQuestion = () => {
                         Wrong answer
                 </Alert>
             </Snackbar>
+
+            <Backdrop className={classes.backdrop} open={loader}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </div>
     )
 
