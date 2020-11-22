d8e7725f3b4fb8fdf870f97ca299af2c5a08d976
Tarun kumar singh <tarunks2512@gmail.com>
2020-11-22 16:37:06 +0530

    center align

--- src/component/quiz-question/question.jsx
+++ src/component/quiz-question/question.jsx
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
 import Option from "./option";
- 
+import Grid from '@material-ui/core/Grid';
+
 const Question = () =>{
 
      const styles = useOverShadowStyles();
 
     return(
-         <div className="options">
+        <Grid
+  container
+  direction="row"
+  justify="center"
+  alignItems="center"
+>
+         <div>
+             <div>
+                 Where is the capital of India ? 
+             </div>
              <Option
                  Option
                  item="Delhi"
@@ -26,6 +36,7 @@ const Question = () =>{
                  item="Delhi"
                  />
          </div>
+         </Grid>
     )
  
 }
